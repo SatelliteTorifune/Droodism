ed288975d6eca06448fdb7c62b95e5983dbe3516
Ame-no-Torifune no Shrine <usamirenko2003@gmail.com>
2025-07-27 18:36:26 +0800

    能用了

--- Assets/Scripts/Craft/Parts/Modifiers/GravityRingData.cs
+++ Assets/Scripts/Craft/Parts/Modifiers/GravityRingData.cs
@@ -15,11 +15,45 @@ namespace Assets.Scripts.Craft.Parts.Modifiers
     public class GravityRingData : PartModifierData<GravityRingScript>
     {
         [SerializeField] [DesignerPropertyToggleButton(Label = "Reverse the Rotation Direction")]
-        private bool isReverse = true;
-
-        public bool IsReverse
+        public bool IsReverse = false;
+        private Vector3 positionOffset1 = Vector3.zero;
+        [SerializeField]
+        [PartModifierProperty]
+        private float currentExtentPercent = 0;
+        [SerializeField]
+        [PartModifierProperty]
+        public float CurrentRotation = 90f;
+        [SerializeField]
+        [DesignerPropertySlider(0.1f, 1f,10, Label = "Extend Speed")]
+        public float ExtendSpeed = 0.2f;
+        [SerializeField] [DesignerPropertySlider(1f, 30f,30, Label = "Deploy Rotation Speed")]
+        private float deployRotationSpeed = 15f;
+        [SerializeField] [DesignerPropertySlider(0.1f, 2f,20, Label = "Main Rotation Speed")]
+        private float rotationSpeed = 1f;
+        
+        
+        public float DeployRotationSpeed
         {
-            get => isReverse;
+            get => deployRotationSpeed;
         }
+
+        public float CurrentExtentPercent
+        {
+            get => currentExtentPercent;
+            set => currentExtentPercent = value;
+        }
+        
+
+        public Vector3 PositionOffset1
+        {
+            get => positionOffset1;
+        }
+
+        public float RotationSpeed
+        {
+            get => rotationSpeed*0.5f;
+            set => rotationSpeed = value;
+        }
+        
     }
 }
